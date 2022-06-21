879bd00afedd410a2ada5f37b9961b7b7ad4c460
jintli <jintli@qq.com>
2022-06-21 20:27:31 +0800

    claim contract

--- contracts/CryptoBlessing.sol
+++ contracts/CryptoBlessing.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "hardhat/console.sol";
 
 contract CryptoBlessing is Ownable {
@@ -27,7 +28,7 @@ contract CryptoBlessing is Ownable {
     }
 
     struct SenderBlessing {
-        string blessingID;
+        address blessingID;
         string blessingImage;
         uint256 sendTimestamp;
         uint256 tokenAmount;
@@ -43,10 +44,11 @@ contract CryptoBlessing is Ownable {
     }
 
     struct ClaimerBlessing {
-        string blessingID;
+        address blessingID;
         string blessingImage;
         uint256 claimTimestamp;
-        uint256 tokenAmount;
+        uint256 claimAmount;
+        uint256 taxAmount;
     }
 
     // 接收者的祝福列表
@@ -59,14 +61,16 @@ contract CryptoBlessing is Ownable {
     struct BlessingClaimStatus {
         address claimer;
         uint256 claimTimestamp;
+        uint256 distributedAmount;
         uint256 claimAmount;
+        uint256 taxAmount;
         uint256 CBTokenAwardToSenderAmount;
     }
 
-    // 祝福的状态列表
-    mapping (string => BlessingClaimStatus[]) blessingClaimStatusMapping;
+    // 祝福的状态列表 blessingID => BlessingClaimStatus[]
+    mapping (address => BlessingClaimStatus[]) blessingClaimStatusMapping;
 
-    function getBlessingClaimingStatus(string memory _blessingID) public view returns (BlessingClaimStatus[] memory) {
+    function getBlessingClaimingStatus(address _blessingID) public view returns (BlessingClaimStatus[] memory) {
         return blessingClaimStatusMapping[_blessingID];
     }
 
@@ -86,13 +90,14 @@ contract CryptoBlessing is Ownable {
 
     function addBlessing(
         string memory image,
+        address blessingOwner,
         string memory description,
         uint256 price,
         uint8 blessingType
     ) public onlyOwner {
         console.log("start to add blessing to the pool!");
         blessingList.push(Blessing(
-            image, description, price, msg.sender, blessingType, block.timestamp
+            image, description, price, blessingOwner, blessingType, block.timestamp
         ));
     }
 
@@ -117,12 +122,17 @@ contract CryptoBlessing is Ownable {
 
     function sendBlessing(
         string memory image,
-        string memory blessingID,
+        address blessingID,
         uint256 tokenAmount,
         uint8 claimQuantity,
         ClaimType claimType
     ) public {
-        console.log("start to send blessing! image:%s", image);
+        console.log("start to send blessing! image:%s, blessingID:%s", image, blessingID);
+        console.log("tokenAmount: %s", tokenAmount);
+        console.log("claimQuantity: %s", claimQuantity);
+        console.log("claimType: %s", uint(claimType));
+        require(0 < tokenAmount, "tokenAmount must be greater than 0");
+        require(0 < claimQuantity && claimQuantity <= 10, "claimQuantity must be greater than 0 and less or equal than 10");
         Blessing memory choosedBlessing;
         for (uint256 i = 0; i < blessingList.length; i ++) {
             if (compareStrings(blessingList[i].image, image)) {
@@ -131,10 +141,10 @@ contract CryptoBlessing is Ownable {
             }
         }
         require(choosedBlessing.price > 0, "Invalid blessing!");
-        require(IERC20(sendTokenAddress).balanceOf(msg.sender) >= tokenAmount, "Your token amount must be greater then you are trying to send");
-        require(IERC20(sendTokenAddress).approve(address(this), tokenAmount));
-        require(IERC20(sendTokenAddress).transferFrom(msg.sender, address(this), tokenAmount));
-
+        require(IERC20(sendTokenAddress).balanceOf(msg.sender) >= tokenAmount + (claimQuantity * choosedBlessing.price), "Your token amount must be greater than you are trying to send!");
+        // require(IERC20(sendTokenAddress).approve(address(this), tokenAmount), "Approve failed!");
+        require(IERC20(sendTokenAddress).transferFrom(msg.sender, address(this), tokenAmount), "Transfer to contract failed!");
+        require(IERC20(sendTokenAddress).transferFrom(msg.sender, choosedBlessing.owner, claimQuantity * choosedBlessing.price), "Transfer to the owner of blessing failed!");
 
         senderBlessingMapping[msg.sender].push(SenderBlessing(
             blessingID,
@@ -144,6 +154,78 @@ contract CryptoBlessing is Ownable {
             claimQuantity,
             claimType
         ));
+    }
+
+    function claimBlessing(
+        address sender,
+        address blessingID,
+        bytes32 hash,
+        bytes memory signature
+    ) public {
+        console.log("start to claim blessing! sender:%s, blessingID:%s", sender, blessingID);
+        require(_verify(hash, signature, blessingID), "Invalid signiture!");
+        console.log("signature is valid!");
+        SenderBlessing[] memory senderBlessings = senderBlessingMapping[sender];
+        require(senderBlessings.length > 0, "There is no blessing found on this sender!");
+        SenderBlessing memory choosedSenderBlessing;
+        for (uint256 i = 0; i < senderBlessings.length; i ++) {
+            if (senderBlessings[i].blessingID == blessingID) {
+                choosedSenderBlessing = senderBlessings[i];
+            }
+        }
+        
+        require(choosedSenderBlessing.tokenAmount > 0, "There is no blessing found on this sender!");
+        BlessingClaimStatus[] memory blessingClaimStatusList = blessingClaimStatusMapping[blessingID];
+        require(blessingClaimStatusList.length < choosedSenderBlessing.claimQuantity, "There is no more blessings!");
+
+
+        uint256 distributedAmount = 0;
+        for (uint256 i = 0; i < blessingClaimStatusList.length; i ++) {
+            require(blessingClaimStatusList[i].claimer != msg.sender, "You have already claimed this blessing!");
+            distributedAmount += blessingClaimStatusList[i].distributedAmount;
+        }
+
+
+        uint256 distributionAmount = 0;
+        if (choosedSenderBlessing.claimType == ClaimType.AVERAGE_CLAIM) {
+            distributionAmount = choosedSenderBlessing.tokenAmount / choosedSenderBlessing.claimQuantity;
+        } else if (choosedSenderBlessing.claimType == ClaimType.RANDOM_CLAIM) {
+            uint randromNumber = _random(choosedSenderBlessing.claimQuantity);
+            uint256 leftQuantity = choosedSenderBlessing.claimQuantity - blessingClaimStatusList.length;
+            if (leftQuantity == 1) {
+                distributionAmount = choosedSenderBlessing.tokenAmount - distributedAmount;
+            } else {
+                distributionAmount = (choosedSenderBlessing.tokenAmount - distributedAmount) / leftQuantity * randromNumber / leftQuantity;
+            }
+        }
+
+        require(IERC20(sendTokenAddress).transfer(msg.sender, distributionAmount / 100 * 95), "Claim the token failed!");
+
+        blessingClaimStatusMapping[blessingID].push(BlessingClaimStatus(
+            msg.sender,
+            block.timestamp,
+            distributionAmount,
+            distributionAmount / 100 * 95,
+            distributionAmount / 100 * 5,
+            10
+        ));
+
+        claimerBlessingMapping[msg.sender].push(ClaimerBlessing(
+            blessingID,
+            choosedSenderBlessing.blessingImage,
+            block.timestamp,
+            distributionAmount / 100 * 95,
+            distributionAmount / 100 * 5
+        ));
+    }
+
+    function _random(uint number) internal view returns(uint){
+        return uint(blockhash(block.number-1)) % number;
+    }
+
+    function _verify(bytes32 data, bytes memory signature, address account) internal pure returns (bool) {
+        address signatureAddress = ECDSA.recover(data, signature);
+        return signatureAddress == account;
     }
 
 }
--- test/cb-test.js
+++ test/cb-test.js
@@ -4,41 +4,45 @@ const { ethers } = require("hardhat");
 
 describe("CryptoBlessing", function () {
 
-    const BUSD_ADDRESS = "0xed24fc36d5ee211ea25a80239fb8c4cfd80f12ee";
-
-    let cbToken, cryptoBlessing;
-
+    let cbToken, BUSD, cryptoBlessing;
 
     var deployCBToken = async function () {
         const CBToken = await ethers.getContractFactory("CBToken");
-        cbToken = await CBToken.deploy();
+        cbToken = await CBToken.deploy();   
         await cbToken.deployed();
     }
 
     var deployCryptoBlessing = async function () {
         const CryptoBlessing = await ethers.getContractFactory("CryptoBlessing");
-        cryptoBlessing = await CryptoBlessing.deploy(BUSD_ADDRESS, cbToken.address);
+        cryptoBlessing = await CryptoBlessing.deploy(BUSD.address, cbToken.address);
         await cryptoBlessing.deployed();
     }
 
-    it("Should append or remove one blessing from the blessing pool?", async function () {
+    var deployBUSD = async function () {
+        const BUSDC = await ethers.getContractFactory("BUSD");
+        BUSD = await BUSDC.deploy();
+        await BUSD.deployed();
+    }
 
+    it("Should append or remove one blessing from the blessing pool?", async function () {
+        const [owner, anotherAddress] = await ethers.getSigners();
         // deploy contracts
         await deployCBToken();
+        await deployBUSD();
         await deployCryptoBlessing();
         
         let allBlessings = await cryptoBlessing.getAllBlessings()
         expect(allBlessings.length).to.equal(0);
 
         // add blessing to the pool
-        const addBlessingTx = await cryptoBlessing.addBlessing("blessing image", "gong xi fa cai", BigInt(1 * 10 ** 18), 1);
+        const addBlessingTx = await cryptoBlessing.addBlessing("blessing image", owner.address, "gong xi fa cai", BigInt(1 * 10 ** 18), 1);
         await addBlessingTx.wait();
         allBlessings = await cryptoBlessing.getAllBlessings()
         expect(allBlessings.length).to.equal(1);
         expect(allBlessings[0].image).to.equal("blessing image");
 
         // add another blessing to the pool
-        const addBlessingTx2 = await cryptoBlessing.addBlessing("make love, not war", "make love, not war", BigInt(9.9 * 10 ** 18), 2);
+        const addBlessingTx2 = await cryptoBlessing.addBlessing("make love, not war", owner.address, "make love, not war", BigInt(9.9 * 10 ** 18), 2);
         await addBlessingTx2.wait();
         allBlessings = await cryptoBlessing.getAllBlessings()
         expect(allBlessings.length).to.equal(2);
@@ -55,20 +59,138 @@ describe("CryptoBlessing", function () {
 
 
 
-    // it("Should not append or remove one blessing from the blessing pool?", async function () {
+    it("Should not append or remove one blessing from the blessing pool?", async function () {
 
-    //     const [owner, anotherAddress] = await ethers.getSigners();
+        const [owner, anotherAddress] = await ethers.getSigners();
 
-    //     // deploy contracts
-    //     await deployCBToken();
-    //     await deployCryptoBlessing();
+        // deploy contracts
+        await deployCBToken();
+        await deployBUSD();
+        await deployCryptoBlessing();
 
-    //     // add blessing to the pool
-    //     await cryptoBlessing.connect(anotherAddress).removeBlessing("test image");
-    //     await cryptoBlessing.connect(anotherAddress).addBlessing("blessing image", "gong xi fa cai", BigInt(1 * 10 ** 18), 1);
-    //     // remove blessing from the pool
-    //     // await cryptoBlessing.connect(anotherAddress).removeBlessing("test image");
-    // });
+        // add blessing to the pool
+        let err = "";
+        try {
+            await cryptoBlessing.connect(anotherAddress).addBlessing("blessing image", owner.address,  "gong xi fa cai", BigInt(1 * 10 ** 18), 1);
+        } catch(e) {
+            err = e.message;
+        }
+        console.log(err)
+        expect(err).to.equal("VM Exception while processing transaction: reverted with reason string 'Ownable: caller is not the owner'");
+        // remove blessing from the pool
+        try {
+            await cryptoBlessing.connect(anotherAddress).removeBlessing("test image");
+        } catch(e) {
+            err = e.message;
+        }
+        expect(err).to.equal("VM Exception while processing transaction: reverted with reason string 'Ownable: caller is not the owner'");
+    });
 
+    it("Should send a blessing?", async function () {
+
+        const [owner, blessingOwner] = await ethers.getSigners();
+        const blessingKeypair = ethers.Wallet.createRandom();
+        // deploy contracts
+        await deployCBToken();
+        await deployBUSD();
+        await deployCryptoBlessing();
+
+        const sendBUSDAmount = BigInt(200 * 10 ** 18);
+        const blessingPrice = BigInt(1 * 10 ** 18);
+        const claimQuantity = 10;
+
+        // 0 allowance
+        const approveBUSDTx = await BUSD.approve(cryptoBlessing.address, BigInt(210 * 10 ** 18));
+        await approveBUSDTx.wait();
+
+        // 1 add blessing to the pool
+        const addBlessingTx = await cryptoBlessing.addBlessing("blessing image", blessingOwner.address, "gong xi fa cai", blessingPrice, 1);
+        await addBlessingTx.wait();
+
+        // 2 check the balance of the sender BUSD = 400
+        let senderBUSD = await BUSD.balanceOf(owner.address);
+        console.log("senderBUSD: ", senderBUSD);
+        // 2 send blessing
+        const sendBlessingTx = await cryptoBlessing.sendBlessing(
+            "blessing image", blessingKeypair.address, 
+            sendBUSDAmount, 
+            claimQuantity,
+            0 
+        );
+        await sendBlessingTx.wait();
+        senderBUSD = await BUSD.balanceOf(owner.address);
+        console.log("after send senderBUSD: ", senderBUSD);
+        expect(senderBUSD).to.equal(BigInt(190 * 10 ** 18));
+        let cbBUSD = await BUSD.balanceOf(cryptoBlessing.address);
+        console.log("after send cbBUSD: ", cbBUSD);
+        expect(cbBUSD).to.equal(BigInt(200 * 10 ** 18));
+        let blessingOwnerBUSD = await BUSD.balanceOf(blessingOwner.address);
+        expect(blessingOwnerBUSD).to.equal(BigInt(10 * 10 ** 18));
+
+        let mySendedBlessings = await cryptoBlessing.getMySendedBlessings()
+        expect(mySendedBlessings.length).to.equal(1);
+        expect(mySendedBlessings[0].blessingID).to.equal(blessingKeypair.address);
+        expect(mySendedBlessings[0].claimType).to.equal(0);
+    });
+
+    it("Should claim the blessing(random)?", async function () {
+
+        const [owner, blessingOwner, claimer] = await ethers.getSigners();
+        const blessingKeypair = ethers.Wallet.createRandom();
+        // const blessingKeypair = web3.eth.accounts.create();
+        // deploy contracts
+        await deployCBToken();
+        await deployBUSD();
+        await deployCryptoBlessing();
+
+        const sendBUSDAmount = BigInt(200 * 10 ** 18);
+        const blessingPrice = BigInt(1 * 10 ** 18);
+        const claimQuantity = 10;
+
+        // 0 allowance
+        const approveBUSDTx = await BUSD.approve(cryptoBlessing.address, BigInt(210 * 10 ** 18));
+        await approveBUSDTx.wait();
+
+        // 1 add blessing to the pool
+        const addBlessingTx = await cryptoBlessing.addBlessing("blessing image", blessingOwner.address, "gong xi fa cai", blessingPrice, 1);
+        await addBlessingTx.wait();
+
+        // 2 send blessing
+        const sendBlessingTx = await cryptoBlessing.sendBlessing(
+            "blessing image", blessingKeypair.address, 
+            sendBUSDAmount, 
+            claimQuantity,
+            0 
+        );
+        await sendBlessingTx.wait();
+        console.log("start to sign the blessing, the private key is: ", blessingKeypair.privateKey);
+        console.log("public key:", blessingKeypair.address);
+        const MESSAGE = web3.utils.sha3('CryptoBlessing');
+        // const signature = await blessingKeypair.signMessage(MESSAGE)
+        const signature = await web3.eth.accounts.sign(MESSAGE, blessingKeypair.privateKey);
+        // const signature = await web3.eth.sign(MESSAGE, blessingKeypair.address);
+        // 3 claim the blessing
+        const claimBlessingTx = await cryptoBlessing.connect(claimer).claimBlessing(
+            owner.address,
+            blessingKeypair.address,
+            toEthSignedMessageHash(MESSAGE),
+            signature.signature,
+        );
+        await claimBlessingTx.wait();
+        let claimerBUSD = await BUSD.balanceOf(claimer.address);
+        expect(claimerBUSD).to.equal(BigInt(200 * 10 ** 18 / 10 * 95 / 100));
+        let myClaimedBlessings = await cryptoBlessing.connect(claimer).getMyClaimedBlessings()
+        expect(myClaimedBlessings.length).to.equal(1);
+        console.log("myClaimedBlessings: ", myClaimedBlessings);
+        let blessingClaimingStatus = await cryptoBlessing.getBlessingClaimingStatus(blessingKeypair.address)
+        expect(blessingClaimingStatus.length).to.equal(1);
+        console.log("blessingClaimingStatus: ", blessingClaimingStatus);
+    });
+
+    function toEthSignedMessageHash (messageHex) {
+        const messageBuffer = Buffer.from(messageHex.substring(2), 'hex');
+        const prefix = Buffer.from(`\u0019Ethereum Signed Message:\n${messageBuffer.length}`);
+        return web3.utils.sha3(Buffer.concat([prefix, messageBuffer]));
+    }
 
 });
