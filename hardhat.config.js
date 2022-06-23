bf5b4b967510d3c6b439ac7da3f48af0f0219a61
jintli <jintli@qq.com>
2022-06-23 16:27:32 +0800

    add next template

--- hardhat.config.js
+++ hardhat.config.js
@@ -27,9 +27,9 @@ module.exports = {
       chainId: 1337
     },
     bsc_testnet: {
-      url: process.env.REACT_APP_BSC_TESTNET_URL,
+      url: "https://apis.ankr.com/4ba236862ab54a55b364dcd322cdb412/807cff1041c516e514318a326153c1f3/binance/full/test",
       chainId: 97,
-      accounts: [process.env.REACT_APP_BSC_TESTNET_OWNER_PRIVATEKEY]
+      accounts: ["ac2d5115cdb6297b6182362c93fefaff3dc6188afcbe1da3dd62002f5fab01b5"]
     }
   },
 };
