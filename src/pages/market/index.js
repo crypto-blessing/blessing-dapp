07b37df41f5535089f7c938d6a1903546b261e68
jintli <jintli@qq.com>
2022-07-16 22:56:26 +0800

    add category page

--- src/pages/market/index.js
+++ src/pages/market/index.js
@@ -7,53 +7,13 @@ import Box from '@mui/material/Box'
 // ** Demo Components Imports
 import BlessingCard2 from 'src/views/cards/BlessingCard2'
 
-import { ethers } from 'ethers'
-import CryptoBlessing from 'src/artifacts/contracts/CryptoBlessing.sol/CryptoBlessing.json'
-import { useWeb3React } from "@web3-react/core"
-import {cryptoBlessingAdreess, BUSDContractAddress} from 'src/@core/components/wallet/address'
 
 import { useEffect, useState } from "react"
-import { Block } from '@mui/icons-material'
 import { Divider, Slider } from '@mui/material'
 
-const Blessings = () => {
-
-    const { active, account, chainId } = useWeb3React()
-    const [blessings, setBlessings] = useState([])
+const Market = () => {
 
     const [categoriesWithItems, setCategoriesWithItems] = useState([])
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    // async function fetchBlessings() {o09
-    //     if (active && chainId != 'undefined' && typeof window.ethereum !== 'undefined') {
-    //         const provider = new ethers.providers.Web3Provider(window.ethereum)
-    //         const cbContract = new ethers.Contract(cryptoBlessingAdreess(chainId), CryptoBlessing.abi, provider.getSigner())
-    //         try {
-    //             setBlessings(await cbContract.getAllBlessings())
-    //         } catch (err) {
-    //             setBlessings([{
-    //                 image: 'gongxifacai.gif',
-    //                 description: 'gong xi fa cai#In every Chinese New Year, the greetings among Chinese',
-    //                 price: BigInt(0.1 * 10 ** 18),
-    //                 owner: '0xdF3e18d64BC6A983f673Ab319CCaE4f1a57C7097',
-    //             }])
-    //             console.log("Error: ", err)
-    //         }
-            
-    //     } else {
-    //         setBlessings([{
-    //             image: 'gongxifacai.gif',
-    //             description: 'gong xi fa cai#In every Chinese New Year, the greetings among Chinese',
-    //             price: BigInt(0.1 * 10 ** 18),
-    //             owner: '0xdF3e18d64BC6A983f673Ab319CCaE4f1a57C7097',
-    //         }])
-    //     }
-    // }
-
-    // useEffect(() => {
-    //     fetchBlessings()
-    // // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, [chainId, account])
 
     useEffect(() => {
         fetch('/api/items')
@@ -70,7 +30,7 @@ const Blessings = () => {
                 <Grid container spacing={6}>
                     <Grid item xs={12}>
                         <Typography variant='h5'>
-                        <Link href=''>
+                        <Link href={'/market/' + item.type}>
                             {item.type}
                         </Link>
                         </Typography>
@@ -91,4 +51,4 @@ const Blessings = () => {
     )
 }
 
-export default Blessings
+export default Market
